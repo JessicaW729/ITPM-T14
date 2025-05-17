dac2ce4c5289e1f87e5bbdbdaa70a7f1a00790d8
JessicaW729 <jessicawaller729@gmail.com>
2025-05-17 13:54:23 +1000

    Update Booking.java
    Created basic template for Booking class

--- Booking.java
+++ Booking.java
@@ -1,9 +1,31 @@
-// this is a placeholder file for the Booking class
-
 public class Booking{
-    // this is a placeholder for the class
+    private int customerId;
+    private int flightId;
+    private String seatNumber;
+    private boolean mealPurchased;
+    private boolean onboardWifiPurchased;
+    private int extraBaggagePurchased;
+    
+    public Booking(int customerId, int flightId){
+        this.customerId = customerId;
+        this.flightId = flightId;
+        this.seatNumber = "not specified";
+        this.mealPurchased = false;
+        this.onboardWifiPurchased = false;
+        this.extraBaggagePurchased = 0;
+    }
 
-    public Booking(){
-        // this is a placeholder for a constructor
+    public Booking(int customerId, int flightId, String seatNumber, boolean mealPurchased, boolean onboardWifiPurchased, int extraBaggagePurchased){
+        this.customerId = customerId;
+        this.flightId = flightId;
+        this.seatNumber = seatNumber;
+        this.mealPurchased = mealPurchased;
+        this.onboardWifiPurchased = onboardWifiPurchased;
+        this.extraBaggagePurchased = extraBaggagePurchased;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%d, %d, %s, %s, %s, %d", customerId, flightId, seatNumber, mealPurchased, onboardWifiPurchased, extraBaggagePurchased);
     }
 }
